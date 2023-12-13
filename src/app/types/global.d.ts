9313a1dbdb3a547f63add8b7873a280b991a2f0a
Alon4ic <o.redjko@gmail.com>
2023-12-13 21:08:27 +0200

    Add babel, add extract plugin

---
+++ src/app/types/global.d.ts
@@ -0,0 +1,17 @@
+declare module "*.scss" {
+  interface IClassName {
+    [className: string]: string;
+  }
+  const className: IClassName;
+  export = className;
+}
+declare module "*.svg" {
+  import React from "react";
+  const SVG: React.VFC<React.SVGProps<SVGAElement>>;
+  export default SVG;
+}
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+
+declare const __IS_DEV__: boolean;
